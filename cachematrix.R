95990c8c34f8005f35f8a24b5f28d3415f7b018e
Manish Sengar <sengar.manish@gmail.com>
2014-07-27 10:04:25 +0530

    R Programming - Cached Matrix - Assignment 2
    R Programming - Cached Matrix - Assignment 2
    Added comments and usage

--- cachematrix.R
+++ cachematrix.R
@@ -1,16 +1,23 @@
+#This function gets and sets a matrix per parameters passed
+#Usage makeCacheMatix(<Matrix passed as paremeter here>)
 makeCacheMatrix <- function(x = matrix()) {
      invMatrix <-NULL
+     
      #Set matrix function
      set<-function(y){
           x <<-y
           invMatrix <<-NULL
      }
+     
      #get matrix function
      get<-function() x
+     
      #set inverse matrix function
      setInverseMatrix<-function(inverse) invMatrix <<- inverse
+     
      #get Inverse matix function
      getInverseMatrix<-function() invMatrix
+     
      list(set=set
           , get=get
           , setInverseMatrix=setInverseMatrix
@@ -18,6 +25,11 @@ makeCacheMatrix <- function(x = matrix()) {
      )
 }
 
+#Cachesolve funtion checks if matrix passed has been inverted in past
+#If yes cached results are returned
+#Else matrix is inverted and returned
+#This function assumes that matrix is invertible
+#Usage cacheSolve(<Invertible matrix to be inverted here>)
 cacheSolve <- function(x, ...) {
      invMatrix <-x$getInverseMatrix()
      #return from cache if already inverted
