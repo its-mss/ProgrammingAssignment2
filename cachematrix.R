cef158433c984e95d28e8c8648ff3bd3ca819da3
Manish Sengar <sengar.manish@gmail.com>
2014-07-27 09:45:49 +0530

    R Programming - Cached matrix - Assignment 2
    R Programming - Cached matrix - Assignment 2
    Submitted by Manish Sengar

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,52 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
 makeCacheMatrix <- function(x = matrix()) {
-
+     invMatrix <-NULL
+     #Set matrix function
+     set<-function(y){
+          x <<-y
+          invMatrix <<-NULL
+     }
+     #get matrix function
+     get<-function() x
+     #set inverse matrix function
+     setInverseMatrix<-function(inverse) invMatrix <<- inverse
+     #get Inverse matix function
+     getInverseMatrix<-function() invMatrix
+     list(set=set
+          , get=get
+          , setInverseMatrix=setInverseMatrix
+          , getInverseMatrix=getInverseMatrix
+     )
 }
-
-
-## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+     invMatrix <-x$getInverseMatrix()
+     #return from cache if already inverted
+     if(!is.null(invMatrix)){
+          message("getting cached data")
+          return(invMatrix)
+     }
+     data <-x$get()
+     #use solve method to get inverse
+     invMatrix <-solve(data, ...)
+     x$setInverseMatrix(invMatrix)
+     invMatrix
 }
+
+#Test results
+#> x = rbind(c(1, 2), c(3, 4))
+#> m = makeCacheMatrix(x)
+#> m$get()
+#[,1] [,2]
+#[1,]    1    2
+#[2,]    3    4
+#First run - no caching
+#> cacheSolve(m)
+#[,1] [,2]
+#[1,] -2.0  1.0
+#[2,]  1.5 -0.5
+#Secind run - cached result returned
+#> cacheSolve(m)
+#getting cached data
+#[,1] [,2]
+#[1,] -2.0  1.0
+#[2,]  1.5 -0.5
